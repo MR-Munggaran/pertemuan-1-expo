aa81a55e988be9bd4d68889a08a189ce3c97419e
Muhammad Rafi Munggaran <ethanjeger123@gmail.com>
2024-06-08 20:56:10 +0700

    first

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Constant from 'expo-constants';
+
+HaloAplikasi = () => {
+  const id = '001'
+  const namaD = 'Rafi'
+  const namaB = 'Munggaran'
+  namaLengkap = () => {
+    return `$(namaD) $(namaB)`
+  }
+  
+}
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    // Latihan 1A
+    // <View style={{ paddingTop: Constant.statusBarHeight}}>
+    //   <View style={{backgroundColor:'salmon',height: 50,padding: 20,}}></View>
+    //   <Text style={{fontSize: 50, fontWeight:'500', color:'black'}}>Selamat Datang {'\n'}Di {'\n'}React Native</Text>
+    //   {/* <Text>{`Selamat \n Datang \n di \n React Native`}</Text> */}
+    // </View>
+    <View style={styles.home}>
+      <Text style={styles.header}>TEST</Text>
     </View>
   );
 }
@@ -13,8 +30,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ECB159',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  home: {
+    paddingTop: Constant.statusBarHeight,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ECB159',
+  },
+  header: {
+    fontSize: 50, 
+    fontWeight:'500', 
+    color:'black'
+  }
 });
